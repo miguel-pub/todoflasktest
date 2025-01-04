1ef11cced864a8644a8790d108bf0441da173b47
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-04 10:14:42 +0100

    route home

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html")
 if __name__ == "__main__":
