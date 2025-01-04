696c7d21f882e65b24b7a616ef36e377592b3dd7
miguel-pub <miguelebersbach+1@outlook.com>
2025-01-04 12:35:08 +0100

    Patch, Delete
    I guess it works now. good practice.

--- api.py
+++ api.py
@@ -40,7 +40,39 @@ class Users(Resource):
         users = UserModel.query.all()
         return users, 201
 
+
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        return user
+    
+    @marshal_with(userFields)
+    def patch(self, id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        user.name = args ["name"]
+        user.email = args["email"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self, id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404, "User not found")
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 200
+        
+    
 api.add_resource(Users, "/api/users/")
+api.add_resource(User, "/api/users/<int:id>")
 
 @app.route("/")
 def home():
